f3b5a4f7a12db8c89d83690077318a83c19df177
antony waweru <anto.waweru2030@gmail.com>
2023-01-05 03:31:54 +0300

    lab done

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 
 // Write your code here
+
+//setting a multiply equation
+
+ let multiply = function(num1,num2){
+    num1 = 2;
+    num2 = 31;
+    console.log (num1*num2);
+} 
+
+multiply()
+
+//generating a random integer
+
+let random = function getRandom() {
+    return Math.random();
+  }
+
+console.log (random())
+
+// equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+
+
+
+mod = function(num3,num4){
+    num3=14;
+    num4=5;
+    return num3%num4;
+} 
+console.log(mod())
+
+//variable that finds the highest number in a set; the value returned should be 20.
+
+max = Math.max(13, 2, 14, 4, 20);
+console.log(max)
